4cef784632903fb99e30a9a090747068bb93c174
vikahaze <vikahaze@users.noreply.github.com>
2025-12-09 06:14:47 +0200

    lint

--- solutions/1071/01.py
+++ solutions/1071/01.py
@@ -4,15 +4,15 @@ class Solution:
         # This is necessary for a common divisor to exist
         if str1 + str2 != str2 + str1:
             return ""
-        
+
         # Find GCD of lengths using Euclidean algorithm
         def gcd(a, b):
             while b:
                 a, b = b, a % b
             return a
-        
+
         # The GCD string will have length equal to GCD of the two string lengths
         gcd_len = gcd(len(str1), len(str2))
-        
+
         # Return the prefix of length gcd_len
         return str1[:gcd_len]
