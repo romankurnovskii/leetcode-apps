f304484e4017a44f938a8c27be6a3a40526d769c
Roman Kurnovskii <r.kurnovskii@gmail.com>
2025-12-13 13:48:47 +0200

    2627

--- solutions/2627/01.py
+++ solutions/2627/01.py
@@ -1,29 +1,14 @@
-# JavaScript solution - Python wrapper
-# Note: This is a JavaScript problem, but we provide a Python wrapper for consistency
-
-"""
-function debounce(fn, t) {
-    let timeoutId;
-    return function(...args) {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => fn.apply(this, args), t);
-    };
-}
-"""
-
-# For Python, we can simulate this
-from threading import Timer
-
 class Solution:
-    def debounce(self, fn, t):
+    def debounce(self, fn: 'Callable', t: int) -> 'Callable':
         timeout_id = None
         
-        def debounced(*args, **kwargs):
+        def debounced(*args):
             nonlocal timeout_id
-            if timeout_id:
-                timeout_id.cancel()
-            timeout_id = Timer(t / 1000.0, lambda: fn(*args, **kwargs))
-            timeout_id.start()
+            # Clear any existing timeout
+            if timeout_id is not None:
+                clearTimeout(timeout_id)
+            
+            # Set new timeout
+            timeout_id = setTimeout(lambda: fn(*args), t)
         
         return debounced
-
