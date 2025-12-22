995400a5523682a73466b58716170962146d94b7
Roman Kurnovskii <r.kurnovskii@gmail.com>
2025-12-22 11:51:16 +0200

    Format Python files with black (pre-commit hook formatting)

--- solutions/960/01.py
+++ solutions/960/01.py
@@ -2,12 +2,12 @@ class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
         n = len(strs)
         m = len(strs[0])
-        
+
         # dp[i] = length of longest valid subsequence ending at column i
         # A valid subsequence means columns that can be kept such that
         # all rows remain sorted
         dp = [1] * m
-        
+
         for i in range(1, m):
             for j in range(i):
                 # Check if column i can come after column j
@@ -18,11 +18,10 @@ class Solution:
                     if strs[k][j] > strs[k][i]:
                         valid = False
                         break
-                
+
                 if valid:
                     dp[i] = max(dp[i], dp[j] + 1)
-        
+
         # The answer is total columns minus the longest valid subsequence
         res = m - max(dp)
         return res
-
