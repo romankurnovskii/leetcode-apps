b968760e6c60ec3e3b826169e4bcdbf07261703b
Roman Kurnovskii <r.kurnovskii@gmail.com>
2025-12-23 13:12:02 +0200

    Format solution files with black (pre-commit hook)

--- solutions/3522/01.py
+++ solutions/3522/01.py
@@ -4,14 +4,14 @@ class Solution:
         visited = [False] * n
         score = 0
         i = 0
-        
+
         while 0 <= i < n and not visited[i]:
             visited[i] = True
-            
+
             if instructions[i] == "jump":
                 i += values[i]
             else:
                 score += values[i]
                 i += 1
-        
+
         return score
--- solutions/3527/01.py
+++ solutions/3527/01.py
@@ -1,20 +1,20 @@
 from collections import defaultdict
 
+
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
         count = defaultdict(int)
-        
+
         # Count each unique response from each day
         for daily in responses:
             # Use set to remove duplicates within each day
             for response in set(daily):
                 count[response] += 1
-        
+
         # Find maximum frequency
         max_count = max(count.values())
-        
+
         # Return lexicographically smallest response with max frequency
         res = min(response for response, cnt in count.items() if cnt == max_count)
-        
-        return res
 
+        return res
